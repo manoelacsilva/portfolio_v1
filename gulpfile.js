ac98b316a87703a175dcbcc3f8184447106442e6
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-11 21:47:13 -0300

    commit inicial, setup do portfolio

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
+};
+
+
+exports.default = styles;
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    // gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+};
