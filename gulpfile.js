9b1e65ef304fa34390fab0e0215af1b7be592560
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-13 02:43:21 -0300

    trabalhando no hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 
 function styles() {
@@ -8,8 +9,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 };
 
+function images() {
+    return gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+};
 
-exports.default = styles;
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     // gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
