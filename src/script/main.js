a0cc4300791a7b84130d628a7b469d1572fb7968
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-13 23:22:11 -0300

    iniciando a section habilidades

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.getElementById("typed-text");
+    const textToType = "Desenvolvedora Front-End";
+    let i = 0;
+
+    function typeText() {
+        textElement.textContent += textToType.charAt(i);
+        i++;
+        if (i < textToType.length) {
+            setTimeout(typeText, 150); // Ajuste o valor para a velocidade da digitação
+        }
+    }
+
+    typeText();
+});
