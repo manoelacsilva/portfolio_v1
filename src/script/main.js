733d6be014b308d5483f7b33f352a76fa3674239
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-10-20 01:00:12 -0300

    pronto o header e o hero do mobile 480px

--- src/script/main.js
+++ src/script/main.js
@@ -140,3 +140,25 @@ document.addEventListener('DOMContentLoaded', function() {
         scrollFunction()
     };
 });
+
+// Menu Hamburguer - Mobile
+
+const activeMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('.header .primary-navgation')
+
+activeMenu.addEventListener('click', () => {
+    activeMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('active')
+})
+
+const menuLinks = document.querySelectorAll('.primary-navgation')
+
+
+for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', () => {
+        if (navMenu.classList.contains('active')) {
+            activeMenu.classList.remove('fa-x');
+            navMenu.classList.remove('active');
+        }
+    });
+}
